89682d2b0a0e202803c874be1979cc972452afde
ahmedalichk <ahmedalichk@gmail.com>
2023-10-03 19:41:20 +0500

    first commit

---
+++ src/app/home/about/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from 'next/link'
+
+
+export default function Contact(){
+    return(
+        <main>
+        <div>Contact Page</div>
+        {/* <div>        <link rel="stylesheet" href="/about"> About Us</link></div> */}
+        </main>
+
+    )
+}
---
+++ src/app/home/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from 'next/link'
+
+
+export default function About(){
+    return(
+        <main>
+            <div className="text-5xl font-bold text-blue-500 flex items-center justify-center">About Page hello</div>
+           {/* <div> <link rel="stylesheet" href="/about"> About Us</link></div> */}
+        </main>
+    )
+}
